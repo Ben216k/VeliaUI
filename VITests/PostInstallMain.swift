a6da17fc45fa18eacd10ea8687a7b604a63a96ed
Ben Sova <65474198+BenSova@users.noreply.github.com>
2021-05-13 15:50:46 -0600

    Update de.lpoj Limitations

--- VITests/PostInstallMain.swift
+++ VITests/PostInstallMain.swift
@@ -26,15 +26,15 @@ struct PostInstallMain: View {
             Spacer()
             HStack {
                 VStack(alignment: .leading, spacing: 7) {
-                    VISimpleCell(t: "Update macOS", d: "Go from your current version of macOS to a newer one, where there's something new.", s: "arrow.clockwise.circle", id: "UPDATE", h: $hovered)
+                    VISimpleCell(t: "Update macOS", d: "Aktualisiere macOS oder den Patcher auf eine neue Version.", s: "arrow.clockwise.circle", id: "UPDATE", h: $hovered)
                     VISimpleCell(t: "Patch Kexts", d: "Kexts provide macOS with it's full functionality. So that everything works like it should.", s: "doc.circle", id: "KEXTS", h: $hovered)
-                    VISimpleCell(t: "Settings", d: "Disable animations, enable graphics switching, show logs and maybe more.", s: "gearshape", id: "SETTINGS", h: $hovered)
+                    VISimpleCell(t: "Settings", d: "Animationen und Grafikkarten Umschaltung deaktivieren, Protokolle und mehr.", s: "gearshape", id: "SETTINGS", h: $hovered)
                 }
                 VStack(alignment: .leading, spacing: 7) {
-                    VISimpleCell(t: "Create Installer", d: "Go from your current version of macOS to a newer one, where there's something new.", s: "externaldrive", id: "INSTALLER", h: $hovered)
+                    VISimpleCell(t: "Create Installer", d: "Erstelle einen gepatchten macOS Big Sur Installer USB-Stick, um macOS etc. (neu)installieren zu können.", s: "externaldrive", id: "INSTALLER", h: $hovered)
 //                    VISimpleCell(t: "Install Recovery", d: "Kexts provide macOS with it's full functionality. So that everything works like it should.", s: "asterisk.circle", id: "RECOVERY", h: $hovered)
-                    VISimpleCell(t: "Wiederherstellungsmodus installieren", d: "Patche das Wiederherstellungslaufwerk (mit CMD-R beim Start aktivieren). Ist nicht immer nützlich.", s: "asterisk.circle", id: "RECOVERY", h: $hovered)
-                    VISimpleCell(t: "About This Mac", d: "Disable animations, enable graphics switching, show logs and maybe more.", s: "info.circle", id: "ABOUT", h: $hovered)
+                    VISimpleCell(t: "Wiederherstellung installieren", d: "Patche die Wiederherstellung (mit CMD-R beim Start starten). Ist nicht immer nützlich.", s: "asterisk.circle", id: "RECOVERY", h: $hovered)
+                    VISimpleCell(t: "About This Mac", d: "Zeige Infos über deinen Mac ohne die Seriennummer. Nützlich um mit deinem Erfolg zu posen.", s: "info.circle", id: "ABOUT", h: $hovered)
                 }
             }.frame(width: 600, alignment: .center).padding(.horizontal, 40)
             Spacer()
